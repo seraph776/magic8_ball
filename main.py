8eb26bf88af7c21c6aea0db24593def8b43c4c3e
seraph776 <seraph776@gmail.com>
2021-07-29 16:33:55 -0400

    Updating main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+"""project: Magic 8 Ball Game, created:7/29/2021, author:seraph★776, email:seraph776@gmail.com"""
+
+from magic8_ball import get_fortune
+
+
+def main():
+    get_fortune()
+
+
+if __name__ == '__main__':
+    main()
